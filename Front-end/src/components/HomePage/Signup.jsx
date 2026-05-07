850c828b74ef9f61116ef3e20e0b1cc1c4b8cf29
Asif Hussain <asifhussain5115@gmail.com>
2026-05-07 10:31:16 +0500

    Fix case-sensitive import in Signup component

--- Front-end/src/components/HomePage/Signup.jsx
+++ Front-end/src/components/HomePage/Signup.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Sign from "./sign";
+import Sign from "./Sign";
 function Signup() {
   return (
     <>
